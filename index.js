6a6636394f55f04e401554b575cb03497349ba2c
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-14 08:42:04 -0300

    Introdução ao JavaScript - Básico II: using methods.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let celular = function(){
     this.cor = "prata";
+
+    this.ligar = function(){
+        console.log("uma ligação")
+        return "ligando";
+    }
 }
 
 let objeto = new celular();
 
-console.log(objeto.cor);
+console.log(objeto.ligar());
